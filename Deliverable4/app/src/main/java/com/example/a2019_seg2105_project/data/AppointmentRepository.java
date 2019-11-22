24965f27723f67f36ecb0ce2d3e91e45168a7213
feimlyang <manlin1119@gmail.com>
2019-11-21 19:30:56 -0500

    update class diagram

---
+++ Deliverable4/app/src/main/java/com/example/a2019_seg2105_project/data/AppointmentRepository.java
@@ -0,0 +1,15 @@
+package com.example.a2019_seg2105_project.data;
+
+public class AppointmentRepository {
+/*search for a walk in clinic by address, working hours, type of services
+* returns a arraylist of hashmap*/
+
+/*book an appointment, should provide all of the appointment info */
+
+/*checkin a appointment if it is booked*/
+
+/*calculate the waiting hours of the appointment?*/
+
+/*rate a service after check in, each rating include a score and a comment*/
+
+}
