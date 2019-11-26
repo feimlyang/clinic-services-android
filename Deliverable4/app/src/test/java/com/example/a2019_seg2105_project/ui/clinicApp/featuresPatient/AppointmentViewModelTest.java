aef16cd6825a390a10aa366917a6341c7c7c7ed2
feimlyang <manlin1119@gmail.com>
2019-11-25 19:48:28 -0500

    update ui

---
+++ Deliverable4/app/src/test/java/com/example/a2019_seg2105_project/ui/clinicApp/featuresPatient/AppointmentViewModelTest.java
@@ -0,0 +1,12 @@
+package com.example.a2019_seg2105_project.ui.clinicApp.featuresPatient;
+
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.*;
+public class AppointmentViewModelTest {
+
+
+
+}
